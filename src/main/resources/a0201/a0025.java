35f740a7e1c97d35c045c7133357d8091326ece2
guoxiaozhuang4 <1095741300@qq.com>
2018-02-02 00:26:11 +0800

    add a new class

---
+++ src/main/resources/a0201/a0025.java
@@ -0,0 +1,17 @@
+/**
+ * 	@date: 2018年2月2日上午12:25:33
+ */
+package a0201;
+
+/**
+ * @author guoxiaozhuang4
+ * @Decription TODO
+ * @date 上午12:25:33
+ */
+public class a0025 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is test for github");
+	}
+}
