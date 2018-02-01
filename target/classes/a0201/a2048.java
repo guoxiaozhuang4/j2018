2c48275badd831df8e9be76c55777456dfa7227e
guoxiaozhuang4 <1095741300@qq.com>
2018-02-02 01:13:05 +0800

    能用struts了

---
+++ target/classes/a0201/a2048.java
@@ -0,0 +1,24 @@
+/**
+ * 	@date: 2018年2月2日上午12:50:20
+ */
+package a0201;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author guoxiaozhuang4
+ * @Decription TODO
+ * @date 上午12:50:20
+ */
+public class a2048 extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+
+		System.out.println("the first user struts");
+		
+		return SUCCESS;
+	}
+
+	
+}
