5af2a4139ebd75107723957fad77902536dd8e5b
guoxiaozhuang4 <1095741300@qq.com>
2018-02-02 00:32:00 +0800

    add a branch dev

---
+++ target/classes/a0201/a0030.java
@@ -0,0 +1,17 @@
+/**
+ * 	@date: 2018年2月2日上午12:30:59
+ */
+package a0201;
+
+/**
+ * @author guoxiaozhuang4
+ * @Decription TODO
+ * @date 上午12:30:59
+ */
+public class a0030 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is a new branch named dev");
+	}
+}
